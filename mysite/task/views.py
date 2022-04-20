b1af68d9587852656c1317c87dffcd3603ad8f3a
raytj14 <reitojo2001@gmail.com>
2022-04-20 23:58:43 +0900

    classbaseview index

--- mysite/task/views.py
+++ mysite/task/views.py
@@ -1,6 +1,13 @@
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world!")
+class CBView(View):
+    def get(self,request):
+        params = {"message":"View function:Get process"}
+        return render(request,"task/index.html",params)
+
+    def post(self,request):
+        params = {"message":"View function:Post process"}
+        return render(request,"task/index.html",params)       
