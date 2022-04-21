02e81cfe882fa009859aa1469bb49549cd983a2d
raytj14 <reitojo2001@gmail.com>
2022-04-21 23:58:18 +0900

    make hellopath in accountsapp to check the class-based view

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth import login as auth_login
+from django.http.response import HttpResponseRedirect
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.views import View
 
-# Create your views here.
+'''
+from .forms import LoginForm
+
+
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            'form': LoginForm(),
+        }
+        return TemplateResponse(request, 'accounts/login.html', context)
+    
+    def  post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        if not form.is_valid():
+            context = {
+                'form': form,
+            }
+            return TemplateResponse(request, 'accounts/login.html', context)
+        
+        user = form.get_user()
+
+        auth_login(request, user)
+
+        return HttpResponseRedirect(reverse('shop:index'))
+
+login = LoginView.as_View()
+'''
+
+class HelloView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            'message': "Hello World",
+        }
+        return TemplateResponse(request, 'accounts/hello.html', context)
+
+hello = HelloView.as_view()
