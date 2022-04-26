0a79a0db7168280ff78ab4b8eca926b423d13bb0
raytj14 <reitojo2001@gmail.com>
2022-04-27 00:43:58 +0900

    add passwordchangeview to configurls

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
+    path('accounts/password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
