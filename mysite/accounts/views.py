22b634b5b4c42448be2594b8dba7e2260bc2a96d
raytj14 <reitojo2001@gmail.com>
2022-04-27 23:25:36 +0900

    add signupviews with valid to appviews

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,22 +1,15 @@
-from django.contrib.auth import login as auth_login
-from django.http.response import HttpResponseRedirect
-from django.template.response import TemplateResponse
-from django.urls import reverse
-from django.views import View
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import SignupForm
 
 class SignupView(CreateView):
     form_class = SignupForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
     
-class HelloView(View):
-    def get(self, request, *args, **kwargs):
-        context = {
-            'message': "Hello World",
-        }
-        return TemplateResponse(request, 'accounts/hello.html', context)
-
-hello = HelloView.as_view()
+    def form_valid(self, form):
+        #self.object に save() されたユーザーオブジェクトが格納される
+        valid = super().form_valid(form)
+        login(self.request, self.object)
+        return valid 
