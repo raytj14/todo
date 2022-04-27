f9cf20d28f2083c78a13c5abe8422d3646931153
raytj14 <reitojo2001@gmail.com>
2022-04-27 22:43:18 +0900

    add passwordreseturl to configurl

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -27,4 +27,8 @@ urlpatterns = [
     path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
     path('accounts/password_change/', views.PasswordChangeView.as_view(), name='password_change'),
     path('accounts/password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('accounts/password_reset/', views.PasswordResetView.as_view(),  name='password_reset'),
+    path('accounts/password_reset/done/', views.PasswordResetDoneView.as_view(),  name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/',  views.PasswordResetConfirmView.as_view(),  name='password_reset_confirm'),
+    path('accounts/reset/done/', views.PasswordResetCompleteView.as_view(),  name='password_reset_complete'), 
 ]
