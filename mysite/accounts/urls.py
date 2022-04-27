4164c165373fcd6fd55551a5ecdc5d11ee9486e7
raytj14 <reitojo2001@gmail.com>
2022-04-27 23:15:02 +0900

    add signupurl to appurls

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello', views.HelloView.as_view() )
+    path('hello', views.HelloView.as_view() ),
+    path('signup/', views.SignupView.as_view(), name='signup'), 
 ]
