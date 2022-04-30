341f605b968495a96883c7646ceae29f211afd26
raytj14 <reitojo2001@gmail.com>
2022-05-01 04:56:43 +0900

    create task model

--- mysite/task/models.py
+++ mysite/task/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    """タスクモデル"""
+
+    todo = models.CharField('Todo',  max_length=100, blank=False)
+    created_at = models.DateTimeField('作成日時', auto_now_add=True)
+    updated_at = models.DateTimeField('作成日時', auto_now=True)
+
+    def __str__(self):
+        return self.todo
