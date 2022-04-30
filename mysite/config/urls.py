cfcbf8b8ae88a649c973508d43ff3632dd404299
raytj14 <reitojo2001@gmail.com>
2022-04-30 16:23:39 +0900

    Email sending settings when registering an account

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -24,12 +24,5 @@ urlpatterns = [
     path('',TemplateView.as_view(template_name='home.html'), name='home'),
     path('accounts/', include('accounts.urls')), 
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
-    path('accounts/password_change/', views.PasswordChangeView.as_view(), name='password_change'),
-    path('accounts/password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('accounts/password_reset/', views.PasswordResetView.as_view(),  name='password_reset'),
-    path('accounts/password_reset/done/', views.PasswordResetDoneView.as_view(),  name='password_reset_done'),
-    path('accounts/reset/<uidb64>/<token>/',  views.PasswordResetConfirmView.as_view(),  name='password_reset_confirm'),
-    path('accounts/reset/done/', views.PasswordResetCompleteView.as_view(),  name='password_reset_complete'), 
+    path('accounts/', include('allauth.urls')),
 ]
