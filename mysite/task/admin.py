b35fd3e1ebedf006588760077c38e8f22b890536
raytj14 <reitojo2001@gmail.com>
2022-05-01 04:59:49 +0900

    todo admin

--- mysite/task/admin.py
+++ mysite/task/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from task.models import Todo
 
 # Register your models here.
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    """Todo管理画面"""
+    pass
