7035801bad6b04b587f25d0a1f08b4d666548890
raytj14 <reitojo2001@gmail.com>
2022-04-30 16:07:00 +0900

    add allauthsetting to INSTALLED_APPS

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites', 
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
