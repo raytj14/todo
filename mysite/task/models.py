cd8da216e61b9240401fb0b943c319409d47780e
raytj14 <reitojo2001@gmail.com>
2022-05-01 22:08:54 +0900

    model classmeta

--- mysite/task/models.py
+++ mysite/task/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     """タスクモデル"""
+    class Meta:
+        db_table = 'todo'
 
     todo = models.CharField('Todo',  max_length=100, blank=False)
     created_at = models.DateTimeField('作成日時', auto_now_add=True)
