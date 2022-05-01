f61b2499af8e7c9c15c9097f948c3b822bf3d97b
raytj14 <reitojo2001@gmail.com>
2022-05-01 22:15:52 +0900

    model migrate

--- mysite/task/models.py
+++ mysite/task/models.py
@@ -6,9 +6,9 @@ class Todo(models.Model):
     class Meta:
         db_table = 'todo'
 
-    todo = models.CharField('Todo',  max_length=100, blank=False)
-    created_at = models.DateTimeField('作成日時', auto_now_add=True)
-    updated_at = models.DateTimeField('更新日時', auto_now=True)
+    todo = models.CharField(verbose_name='Todo',  max_length=100, blank=False)
+    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
     def __str__(self):
         return self.todo
